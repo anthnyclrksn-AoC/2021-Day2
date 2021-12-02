eb81f13c323ea8079bd13d9fb4c54ce42da907c6
UP2024320 <86617698+UP2024320@users.noreply.github.com>
2021-12-02 11:00:33 +0000

    update code.js to have part 2

--- src/code.js
+++ src/code.js
@@ -1,5 +1,6 @@
 let fs = require('fs');
 
+//Part 1
 try {
     let data = fs.readFileSync('./src/currentPosition.txt', 'utf8');
     let splitData = data.split('\n');
@@ -20,4 +21,31 @@ try {
     console.log(Number(depth)*Number(distance))
 } catch (error) {
     console.log(`Error: ${error.stack}`);
+};
+
+//Part 2
+try{
+    let data = fs.readFileSync('./src/currentPosition.txt', 'utf8');
+    let splitData = data.split('\n');
+    let depth = 0;
+    let distance = 0;
+    let aim = 0
+    for(let i = 0; i < splitData.length; i++) {
+        let instruction = splitData[i].split(' ');
+        if(instruction[0] === 'forward') {
+            distance = distance + Number(instruction[1]);
+            depth = depth + (Number(instruction[1])*Number(aim))
+        } if(instruction[0] === 'back') {
+            distance = distance - Number(instruction[1])
+        } if(instruction[0] === 'up') {
+            aim = aim - Number(instruction[1]);
+            //depth = depth - Number(instruction[1])
+        } if(instruction[0] === 'down') {
+            aim = aim + Number(instruction[1]);
+            //depth = depth + Number(instruction[1])
+        };
+    };
+    console.log(Number(depth)*Number(distance));
+} catch (error) {
+    console.log(`Error: ${error.stack}.`);
 };
